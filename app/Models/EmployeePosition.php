e43ff0fd1968139edef196a82785ac1cee4a3d48
Vinicius-1307 <viniciusjose9@outlook.com>
2024-03-14 16:56:02 -0300

    feat: created the models

---
+++ app/Models/EmployeePosition.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EmployeePosition extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'position_id',
+        'employee_id',
+        'performance_note'
+    ];
+
+    public function position()
+    {
+        return $this->belongsTo(Positions::class);
+    }
+
+    public function employee()
+    {
+        return $this->belongsTo(Employees::class);
+    }
+}
---
+++ app/Models/Units.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Units extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'fantasy_name',
+        'company_name',
+        'cnpj'
+    ];
+}
