72b8a96b6563735b2de5226fce4efd98528b8c37
Vinicius-1307 <viniciusjose9@outlook.com>
2024-03-15 17:31:47 -0300

    feat: created exports for excel tables

---
+++ app/Exports/EmployeeNotesExport.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\EmployeePosition;
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+
+class EmployeeNotesExport implements FromCollection, WithHeadings
+{
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    public function collection()
+    {
+        $employees = EmployeePosition::with(['employee.unit', 'position'])->get();
+
+        $formattedData = $employees->map(function ($employeePosition) {
+            return [
+                'Nome' => $employeePosition->employee->name,
+                'CPF' => $employeePosition->employee->cpf,
+                'Email' => $employeePosition->employee->email,
+                'Unidade' => $employeePosition->employee->unit->fantasy_name,
+                'Cargo' => $employeePosition->position->position,
+                'Nota de Desempenho' => $employeePosition->performance_note,
+            ];
+        });
+
+        return new Collection($formattedData->values());
+    }
+
+     /**
+     * @return array
+     */
+    public function headings(): array
+    {
+        return [
+            'Nome',
+            'CPF',
+            'Email',
+            'Unidade',
+            'Cargo',
+            'Nota de Desempenho',
+        ];
+    }
+}
