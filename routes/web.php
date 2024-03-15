7f08574308200900468725ed0b56e8ccafea0040
Vinicius-1307 <viniciusjose9@outlook.com>
2024-03-14 23:02:44 -0300

    feat: created routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Employee\EmployeeController;
+use App\Http\Controllers\Position\PositionController;
+use App\Http\Controllers\Units\UnitController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +17,38 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.app');
 });
+
+Route::controller(UnitController::class)
+    ->prefix('units')
+    ->group(function () {
+        Route::post('/', 'create')->name('units.create');
+        Route::get('/', 'getUnits')->name('get.units');
+    });
+
+Route::controller(EmployeeController::class)
+    ->prefix('employees')
+    ->group(function () {
+        Route::post('/', 'create')->name('employee.create');
+        Route::put('/', 'updateEvaluation')->name('employee.update');
+        Route::get('/', 'getEmployees')->name('get.employee');
+    });
+
+Route::controller(PositionController::class)
+    ->prefix('positions')
+    ->group(function () {
+        Route::get('/', 'getPositions')->name('get.positions');
+    });
+
+// Unidades
+Route::get('/units/create', function () {
+    return view('units.create');
+})->name('unit.index');
+// Colaboradores
+Route::get('/employee/create', function () {
+    return view('employees.create');
+})->name('employee.index');
+Route::get('/employee/evaluation', function () {
+    return view('employees.evaluation');
+})->name('employee.evaluation');
