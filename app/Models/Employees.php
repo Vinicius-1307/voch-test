395ee9f0a09116b2be7c4af940df20f4cc17baf3
Vinicius-1307 <viniciusjose9@outlook.com>
2024-03-14 23:04:53 -0300

    feat: created functions for the unit and employees models

--- app/Models/Employees.php
+++ app/Models/Employees.php
@@ -7,6 +7,36 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employees extends Model
 {
+    public function createEmployee(array $data): bool
+    {
+        $this->unit_id = $data['unit_id'];
+        $this->name = $data['name'];
+        $this->cpf = $data['cpf'];
+        $this->email = $data['email'];
+        $employee = $this->save();
+
+        $employeePosition = new EmployeePosition();
+        $employeePosition->employee_id = $this->id;
+        $employeePosition->position_id = $data['position_id'];
+        $employeePosition->save();
+
+        return $employee ? true : false;
+    }
+
+    public function updatePerformance(array $data): bool
+    {
+        dd($data);
+        $employeePosition = EmployeePosition::where('employee_id', $this->id)->first();
+
+        if ($employeePosition) {
+            $employeePosition->performance_note = $data['performance_note'];
+            $employeePosition->save();
+            return true;
+        }
+
+        return false;
+    }
+
     use HasFactory;
 
     protected $fillable = [
--- app/Models/Units.php
+++ app/Models/Units.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Units extends Model
 {
+
+    public function createUnit(array $data):bool
+    {
+        $unit = $this->save($data);
+        return $unit ? true : false;
+    }
+
     use HasFactory;
 
     protected $fillable = [
