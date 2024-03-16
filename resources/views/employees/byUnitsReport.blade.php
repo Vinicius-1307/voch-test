3d0d8d8a060cf916230ef44d7a102fff1c1b8083
Vinicius-1307 <viniciusjose9@outlook.com>
2024-03-16 12:57:17 -0300

    feat: created a view for unit reporting

---
+++ resources/views/employees/byUnitsReport.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+    <h2>Relatórios de Unidades</h2>
+
+    <div class="btn-group" role="group" aria-label="Relatórios">
+        <a href="{{ route('export_by_units.excel') }}" class="btn btn-success mx-2 rounded">Exportar para Excel</a>
+    </div>
+
+    <div id="reportContent"></div>
+@endsection
+
+@push('scripts')
+    <script>
+        $(document).ready(function() {
+            $.get('{{ route('get.by_units') }}', function(data) {
+                console.log(data)
+                $('#reportContent').html(data);
+            });
+        });
+    </script>
+@endpush
