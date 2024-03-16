d205f22d4e7709d3d5536fc4c76d9807d6ce0b03
Vinicius-1307 <viniciusjose9@outlook.com>
2024-03-16 13:15:24 -0300

    fix: organizing the routes

--- routes/web.php
+++ routes/web.php
@@ -25,22 +25,46 @@ Route::controller(UnitController::class)
     ->group(function () {
         Route::post('/', 'create')->name('units.create');
         Route::get('/', 'getUnits')->name('get.units');
+        //Route for view
+        Route::get('/create', function () {
+            return view('units.create');
+        })->name('unit.index');
     });
 
 Route::controller(EmployeeController::class)
     ->prefix('employees')
     ->group(function () {
+        //Routes for controllers
         Route::post('/', 'create')->name('employee.create');
         Route::put('/', 'updateEvaluation')->name('employee.update');
         Route::get('/', 'getEmployees')->name('get.employee');
         Route::get('/top-performers', 'topPerformers')->name('get.top_performers');
         Route::get('/get-all', 'getAll')->name('get.all_employees');
         Route::get('/by-units', 'getAllByUnit')->name('get.by_units');
+        //Routes for views
+        Route::get('/create', function () {
+            return view('employees.create');
+        })->name('employee.index');
 
+        Route::get('/evaluation', function () {
+            return view('employees.evaluation');
+        })->name('employee.evaluation');
+
+        Route::get('/allEmployeesReport', function () {
+            return view('employees.allEmployeesReport');
+        })->name('employee.allEmployeesReport');
+
+        Route::get('/byNotesReport', function () {
+            return view('employees.byNotesReport');
+        })->name('employee.byNotesReport');
+
+        Route::get('/byUnitsReport', function () {
+            return view('employees.byUnitsReport');
+        })->name('employee.byUnitsReport');
+        //Routes for excel export
         Route::get('/export-excel/by-performance', 'exportEmployeesNotes')->name('export_employees_note.excel');
         Route::get('/export-excel/all-employees', 'exportAllEmployees')->name('export_all_employees.excel');
         Route::get('/export-excel/by-units', 'exportUnitsWithEmployees')->name('export_by_units.excel');
-        Route::get('/export-pdf', 'export')->name('export.pdf');
     });
 
 Route::controller(PositionController::class)
@@ -48,27 +72,3 @@ Route::controller(PositionController::class)
     ->group(function () {
         Route::get('/', 'getPositions')->name('get.positions');
     });
-
-Route::get('/units/create', function () {
-    return view('units.create');
-})->name('unit.index');
-
-Route::get('/employee/create', function () {
-    return view('employees.create');
-})->name('employee.index');
-
-Route::get('/employee/evaluation', function () {
-    return view('employees.evaluation');
-})->name('employee.evaluation');
-
-Route::get('/employee/allEmployeesReport', function () {
-    return view('employees.allEmployeesReport');
-})->name('employee.allEmployeesReport');
-
-Route::get('/employee/byNotesReport', function () {
-    return view('employees.byNotesReport');
-})->name('employee.byNotesReport');
-
-Route::get('/employee/byUnitsReport', function () {
-    return view('employees.byUnitsReport');
-})->name('employee.byUnitsReport');
