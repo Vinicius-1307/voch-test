4d291581260f0de1dc339d5c75fc2ed0f21986ce
Vinicius-1307 <viniciusjose9@outlook.com>
2024-03-16 17:09:19 -0300

    fix: removed an error

--- resources/views/tables/byUnit.blade.php
+++ resources/views/tables/byUnit.blade.php
@@ -1,26 +1,20 @@
-@extends('layouts.app')
-
-@section('content')
-    <h2>Total de Colaboradores por Unidade</h2>
-
-    <table class="table">
-        <thead>
+<table class="table">
+    <thead>
+        <tr>
+            <th>Nome Fantasia</th>
+            <th>Razão Social</th>
+            <th>CNPJ</th>
+            <th>Quantidade de Colaboradores</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($units as $unit)
             <tr>
-                <th>Nome Fantasia</th>
-                <th>Razão Social</th>
-                <th>CNPJ</th>
-                <th>Quantidade de Colaboradores</th>
+                <td>{{ $unit->fantasy_name }}</td>
+                <td>{{ $unit->company_name }}</td>
+                <td>{{ $unit->cnpj }}</td>
+                <td>{{ $unit->employees_count }}</td>
             </tr>
-        </thead>
-        <tbody>
-            @foreach ($units as $unit)
-                <tr>
-                    <td>{{ $unit->fantasy_name }}</td>
-                    <td>{{ $unit->company_name }}</td>
-                    <td>{{ $unit->cnpj}}</td>
-                    <td>{{ $unit->employees_count }}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-@endsection
+        @endforeach
+    </tbody>
+</table>
